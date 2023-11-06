05e4bc0782a3f4c2de0f6d1813adecf55a60e51a
Matthieu Mota <matthieumota@gmail.com>
2023-11-06 16:24:33 +0100

    Fix

--- 00-recap/exercices/bases.php
+++ 00-recap/exercices/bases.php
@@ -4,10 +4,10 @@
 
 echo '<h1>Majeur</h1>';
 
-$age = 31;
+$age = 16;
 
 // 1ère version
-if ($age > 18) {
+if ($age >= 18) {
     echo 'Vous pouvez entrer';
 } else {
     echo 'Interdit';
@@ -16,7 +16,7 @@ if ($age > 18) {
 echo '<br>';
 
 // 2ème version
-if ($age > 18) {
+if ($age >= 18) {
     echo 'Vous pouvez entrer';
 } else if ($age >= 16) {
     echo 'Vous êtes presque majeur';
@@ -41,8 +41,8 @@ $result3 = round(($number3 + $number2) / $number1, 2);
 
 echo "$number1 + $number2 + $number3 = $result1 <br>";
 echo "$number1 x ($number3 - $number2) = $result2 <br>";
-echo "($number3 + $number2) / $number1 = $result3";
+echo "($number3 + $number2) / $number1 = $result3 <br>";
 
-if ($result1 == 20 || $result2 == 20 || $result3 == 20) {
+if ($result1 < 20 || $result2 < 20 || $result3 < 20) {
     echo 'Une des opérations renvoie moins de 20';
 }
