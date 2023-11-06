a596d59b08b455cad9bc14e80f252a5fb05454a3
Matthieu Mota <matthieumota@gmail.com>
2023-11-06 11:04:43 +0100

    wip form

--- 00-recap/ajout.php
+++ 00-recap/ajout.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Ajouter un livre';
+$title = 'Ajouter un livre'; // Titre de la page
 
 // Récupérer les données
 $btitle = $_POST['title'] ?? null;
@@ -16,7 +16,29 @@ if (!empty($_POST)) {
         $errors['title'] = 'Le titre est invalide.';
     }
 
-    // @todo Vérifier le reste
+    if ($price < 1 || $price > 100) {
+        $errors['price'] = 'Le prix est invalide.';
+    }
+
+    if (!empty($discount) && ($discount > 100 || $discount < 0)) {
+        $errors['discount'] = 'La promotion est invalide.';
+    }
+
+    if (strlen($isbn) != 10 && strlen($isbn) != 13) {
+        $errors['isbn'] = 'L\'ISBN est invalide.';
+    }
+
+    if (empty($author)) {
+        $errors['author'] = 'L\'auteur est invalide.';
+    }
+
+    // Valider une date
+    // $publishedAt = '2023-11-06';
+    $checked = explode('-', $publishedAt); // [2023, 11, 06]
+    // (int) permet de caster une chaine en entier
+    if (!checkdate($checked[1] ?? 0, $checked[2] ?? 0, (int) $checked[0])) {
+        $errors['published_at'] = 'La date est invalide.';
+    }
 
     if (empty($errors)) {
         // @todo Faire un insert, redirection, message session ?
@@ -27,38 +49,37 @@ require 'partials/header.php';
 ?>
 
     <div class="max-w-5xl mx-auto px-3">
+        <?php if (!empty($errors)) { ?>
         <div class="bg-red-300 p-5 rounded border border-red-800 text-red-800 my-4">
-            <p>Le titre est invalide.</p>
-            <p>Le prix est invalide.</p>
-            <p>L'ISBN est invalide.</p>
-            <p>L'auteur est invalide.</p>
-            <p>La date est invalide.</p>
-            <p>L'image est invalide.</p>
+            <?php foreach ($errors as $error) { ?>
+            <p><?= $error; ?></p>
+            <?php } ?>
         </div>
+        <?php } ?>
         <form action="" method="post" class="w-1/2 mx-auto" enctype="multipart/form-data">
             <div class="mb-4">
                 <label for="title" class="block">Titre *</label>
-                <input type="text" name="title" id="title" class="border-0 border-b focus:ring-0 w-full" value="">
+                <input type="text" name="title" id="title" class="border-0 border-b focus:ring-0 w-full" value="<?= $btitle; ?>">
             </div>
             <div class="mb-4">
                 <label for="price" class="block">Prix *</label>
-                <input type="text" name="price" id="price" class="border-0 border-b focus:ring-0 w-full" value="">
+                <input type="text" name="price" id="price" class="border-0 border-b focus:ring-0 w-full" value="<?= $price; ?>">
             </div>
             <div class="mb-4">
                 <label for="discount" class="block">Promotion</label>
-                <input type="text" name="discount" id="discount" class="border-0 border-b focus:ring-0 w-full" value="">
+                <input type="text" name="discount" id="discount" class="border-0 border-b focus:ring-0 w-full" value="<?= $discount; ?>">
             </div>
             <div class="mb-4">
                 <label for="isbn" class="block">ISBN *</label>
-                <input type="text" name="isbn" id="isbn" class="border-0 border-b focus:ring-0 w-full" value="">
+                <input type="text" name="isbn" id="isbn" class="border-0 border-b focus:ring-0 w-full" value="<?= $isbn; ?>">
             </div>
             <div class="mb-4">
                 <label for="author" class="block">Auteur *</label>
-                <input type="text" name="author" id="author" class="border-0 border-b focus:ring-0 w-full" value="">
+                <input type="text" name="author" id="author" class="border-0 border-b focus:ring-0 w-full" value="<?= $author; ?>">
             </div>
             <div class="mb-4">
                 <label for="published_at" class="block">Publié le *</label>
-                <input type="date" name="published_at" id="published_at" class="border-0 border-b focus:ring-0 w-full" value="">
+                <input type="date" name="published_at" id="published_at" class="border-0 border-b focus:ring-0 w-full" value="<?= $publishedAt; ?>">
             </div>
             <div class="mb-4">
                 <label for="image" class="block mb-2">Image *</label>
