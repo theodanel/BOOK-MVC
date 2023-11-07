0d7a162ae4f4c04475faabef720f348a10ae6b30
Matthieu Mota <matthieumota@gmail.com>
2023-11-07 10:22:35 +0100

    Table with legend

--- 00-recap/exercices/grille.php
+++ 00-recap/exercices/grille.php
@@ -10,10 +10,18 @@
             border-collapse: collapse;
         }
 
-        td {
+        th, td {
             text-align: center;
             width: 25px;
             height: 25px;
+        }
+
+        .legend-right {
+            border-right: 1px solid #000;
+        }
+
+        .legend-bottom {
+            border-bottom: 1px solid #000;
         }
     </style>
 </head>
@@ -21,8 +29,15 @@
     <h1>Grille de multiplication</h1>
 
     <table>
+        <tr>
+            <th class="legend-bottom legend-right">x</th>
+            <?php for ($i = 0; $i <= 10; $i++) { ?>
+                <th class="legend-bottom"><?= $i; ?></th>
+            <?php } ?>
+        </tr>
         <?php for ($row = 0; $row <= 10; $row++) { ?>
             <tr>
+                <th class="legend-right"><?= $row; ?></th>
                 <?php for ($col = 0; $col <= 10; $col++) { ?>
                     <td><?= $row * $col; ?></td>
                 <?php } ?>
