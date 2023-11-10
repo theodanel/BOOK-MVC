db4bcf559025b553e41ed8c07eb8330ff23204dd
Matthieu Mota <matthieumota@gmail.com>
2023-11-10 16:40:36 +0100

    Exo model

--- 06-mvc/src/Controller/MovieController.php
+++ 06-mvc/src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace M2i\Mvc\Controller;
 
+use M2i\Mvc\Model\Movie;
 use M2i\Mvc\View;
 
 class MovieController
@@ -9,32 +10,7 @@ class MovieController
     public function list()
     {
         $title = 'Films';
-        $movies = [
-            [
-                'id' => 1,
-                'title' => 'Shrek Forever After',
-                'duration' => 93,
-                'image' => 'https://image.tmdb.org/t/p/original/6HrfPZtKcGmX2tUWW3cnciZTaSD.jpg',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Planes',
-                'duration' => 91,
-                'image' => 'https://image.tmdb.org/t/p/original/9uqCaPEIep4iBG3U4AqSP20LGjq.jpg',
-            ],
-            [
-                'id' => 3,
-                'title' => 'La Pat\' Patrouille',
-                'duration' => 85,
-                'image' => 'https://image.tmdb.org/t/p/original/iuEmUPqigSBFdu8vRZvVA4parQg.jpg',
-            ],
-            [
-                'id' => 4,
-                'title' => 'Gran Turismo',
-                'duration' => 95,
-                'image' => 'https://image.tmdb.org/t/p/original/vRIHkkuI6FKqUHPJbABbNLRM3VI.jpg',
-            ],
-        ];
+        $movies = Movie::all();
         // @todo le stagiaire a oublié un moyen d'afficher la durée du film au format 1h31 pour 91.
 
         // & permet de faire une référence entre movie et movies
